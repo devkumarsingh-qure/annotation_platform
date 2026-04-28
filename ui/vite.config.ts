de71739709f390f3c055f7107057fb5d1eca0865
Dev Kumar Singh <devkumar.singh@qure.ai>
2026-04-29 01:52:57 +0530

    config

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -2,10 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteCommonjs()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteCommonjs(),
+    nodePolyfills()
+  ],
   optimizeDeps: {
     exclude: ["@cornerstonejs/dicom-image-loader", "@qureai/react-dicom-viewer"],
     include: ["xmlbuilder2", "dicom-parser"],
