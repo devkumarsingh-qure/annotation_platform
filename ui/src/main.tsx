edabfe063c4f52d7ceaf9ceb2bb609a1afa3044a
Dev Kumar Singh <devkumar.singh@qure.ai>
2026-05-08 21:22:20 +0530

    dev toosl

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -3,10 +3,8 @@ import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 import App from "./App";
 
-if (import.meta.env.DEV && "ontouchstart" in window) {
-  import("eruda").then((eruda) => {
-    eruda.default.init();
-  });
-}
+import("eruda").then((eruda) => {
+  eruda.default.init();
+});
 
 createRoot(document.getElementById("root")!).render(<App />);
