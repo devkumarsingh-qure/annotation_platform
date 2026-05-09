39269fb8e6622952cb3dae0ab3e2dfc3df5e8bc7
Dev Kumar Singh <devkumar.singh@qure.ai>
2026-05-09 22:51:25 +0530

    user check

--- ui/src/components/FloatingActions/index.tsx
+++ ui/src/components/FloatingActions/index.tsx
@@ -1,7 +1,15 @@
+import { useContext } from "react";
 import AboutButton from "../AboutButton";
 import FeedbackButton from "../FeedbackButton";
+import { AuthContext } from "../../contexts/auth/authContext";
 
 function FloatingActions() {
+  const { user } = useContext(AuthContext);
+
+  if (!user) {
+    return null;
+  }
+
   return (
     <div className="fixed right-0 top-1/2 z-30 flex -translate-y-1/2 flex-col items-end gap-2">
       <AboutButton />
